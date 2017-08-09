8593d7d6e0016604bf7f3ee023071c247cf9f20e
System <none>
2017-08-09 21:31:27 +0000

    debugging, finish rgb/hex functions

--- test.js
+++ test.js
@@ -7,15 +7,46 @@ const colorConversionThing = require('./code.js')
 
 QUnit.test( 'rgb to hex', function( assert ) {
   const converted = colorConversionThing.rgbToHex( { r: 255, g: 255, b: 255 } )
-  assert.equal( converted.toUpperCase(), '#FFFFFF' )      
+  try {
+    assert.equal( converted.toUpperCase(), '#FFFFFF' )      
+  } catch(err) {
+    console.log(err)
+  }
 })
 
-// TODO - test r g b props with single digit values to hex ^^^
+QUnit.test( 'rgb to hex - single digits', function( assert ) {
+  const converted = colorConversionThing.rgbToHex( { r: 1, g: 1, b: 45 } )
+  try {
+    assert.equal( converted.toUpperCase(), '#01012D' )
+  } catch(err) {
+    console.log(err)
+  }
+})
+
+QUnit.test( 'rgb to hex - values that are 0', function( assert ) {
+  const converted = colorConversionThing.rgbToHex( { r: 0, g: 0, b: 0 } )
+  try {
+    assert.equal( converted.toUpperCase(), '#000000' )
+  } catch(err) {
+    console.log(err)
+  }
+})
 
 QUnit.test( 'hex to rgb', function( assert ) {
   const converted = colorConversionThing.hexToRGB( '#FFFFFF' )
-  assert.propEqual( converted, { r: 255, g: 255, b: 255 } ) // thanks to 4682b4 on yt
+  try {
+    assert.propEqual( converted, { r: 255, g: 255, b: 255 } ) // thanks to 4682b4 on yt
+  } catch(err) {
+    console.log(err)
+  }
 })
+
+// todo: test single digit hex values (eg. #fff)
+
+
+
+
+// todo: try/catch on below tests
 
 QUnit.test( 'colorname to rgb', function( assert ) {
   const converted = colorConversionThing.colornameToRGB( 'papayawhip' )
